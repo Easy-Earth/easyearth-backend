3351a8f20cb8ae2bad78bb5b850b98e3cd69f527
filialove <filialove12@gmail.com>
2026-01-28 16:58:30 +0900

    태그 추가
    1

--- restfulProject/src/main/java/com/kh/spring/quiz/controller/QuizController.java
+++ restfulProject/src/main/java/com/kh/spring/quiz/controller/QuizController.java
@@ -1,6 +1,8 @@
 package com.kh.spring.quiz.controller;
 
 import com.kh.spring.quiz.service.QuizService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/quiz")
 @RequiredArgsConstructor
+@Tag(name = "퀴즈 API", description = "퀴즈 API")
 public class QuizController {
 
     private final QuizService quizService;
 
+    @Operation(summary = "퀴즈 조회", description = "난이도별 퀴즈를 조회하는 API(Easy,Normal,Hard)")
     @org.springframework.web.bind.annotation.GetMapping("/{difficulty}")
     public java.util.List<com.kh.spring.quiz.model.vo.Quiz> getQuizByDifficulty(
             @org.springframework.web.bind.annotation.PathVariable("difficulty") String difficulty) {
