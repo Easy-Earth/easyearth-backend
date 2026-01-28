59d14c479a00804cd464ae16bf21d7d74009e978
KJH-15123 <kim50504376@gmail.com>
2026-01-28 12:19:44 +0900

    [feat] 재미나이 api/pom 디팬던시 추가

---
+++ restfulProject/src/main/java/com/kh/spring/chat/ChatController.java
@@ -0,0 +1,31 @@
+package com.kh.spring.chat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/chat")
+@Tag(name = "채팅", description = "채팅 관련 API")
+@Slf4j
+public class ChatController {
+
+    @Operation(summary = "채팅 테스트", description = "채팅 테스트 API")
+
+    @GetMapping("/test")
+    public ResponseEntity<?> getChatRoomList() {
+        log.info("테스트 주소");
+
+        return ResponseEntity.ok("테스트 성공");
+    }
+
+}
