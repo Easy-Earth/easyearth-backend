17c2156472f2641f522c7b2d05e06a0b7df9d97a
rlawogud02 <wogud020913@naver.com>
2026-01-30 10:03:36 +0900

    [feat]ddd

---
+++ restfulProject/src/main/java/com/kh/spring/item/model/service/ItemServiceImpl.java
@@ -0,0 +1,56 @@
+package com.kh.spring.item.model.service;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.spring.item.model.dao.ItemDao;
+import com.kh.spring.item.model.vo.ItemVO;
+import com.kh.spring.item.model.vo.UserItemsVO;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+	
+	@Autowired
+	private ItemDao dao;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	//포인트상점 아이템 조회
+	@Override
+	public List<ItemVO> storeItem() {
+		
+		return dao.storeItem(sqlSession);
+	}
+	
+	//포인트상점 보유중인 아이템 조회
+	@Override
+	public List<ItemVO> storeMyItem(int memberId) {
+		
+		return dao.storeMyItem(sqlSession,memberId);
+	}
+	
+	//카테고별 아이템 조회
+	@Override
+	public List<ItemVO> itemCategories(String category) {
+		
+		return dao.itemCategories(sqlSession,category);
+	}
+	
+	//포인트상점 아이템 구매
+	@Override
+	public int buyItem(UserItemsVO userItemsVO) {
+		
+		return dao.buyItem(sqlSession,userItemsVO);
+	}
+	
+	
+	 
+	
+
+	
+	
+}
