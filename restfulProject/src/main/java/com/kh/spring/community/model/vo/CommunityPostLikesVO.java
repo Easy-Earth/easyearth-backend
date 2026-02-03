d1fc6147053622ac689c75a17712baae3d16b68e
juhyeon1202 <wngus031202@gmail.com>
2026-02-03 11:43:18 +0900

    [FEAT] / 커뮤니티 좋아요 기능
    커뮤니티 게시글 & 댓글에 좋아요 기능

--- restfulProject/src/main/java/com/kh/spring/community/model/vo/CommunityPostLikesVO.java
+++ restfulProject/src/main/java/com/kh/spring/community/model/vo/CommunityPostLikesVO.java
@@ -17,10 +17,12 @@ public class CommunityPostLikesVO {
 //	POST_ID	NUMBER	No		2	대상 게시글 ID (FK)
 //	MEMBER_ID	NUMBER	No		3	좋아요 누른 회원 ID (FK)
 //	CREATED_AT	DATE	No	SYSDATE 	4	누른 시간
+//	STATUS	VARCHAR2(1 BYTE)	No	'Y' 	5	좋아요 상태 (Y: 선택, N: 취소)
 	
 	private int plId;
 	private int postId;
 	private int memberId;
 	private Date createdAt;
+	private String status;
 	
 }
