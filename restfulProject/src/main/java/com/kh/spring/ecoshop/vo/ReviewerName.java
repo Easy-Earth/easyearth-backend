b5d0e818ce84369693885beb88babb1797e297a2
JihuN126 <dbwlgns1208@naver.com>
2026-02-05 15:04:03 +0900

    리뷰 수정 컨트롤러 수정

--- restfulProject/src/main/java/com/kh/spring/ecoshop/vo/ReviewerName.java
+++ restfulProject/src/main/java/com/kh/spring/ecoshop/vo/ReviewerName.java
@@ -4,16 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
 public class ReviewerName {
-//    SHOP_ID, RATING, CONTENT, NAME
+    int esrId;
     int shopId;
     String rating;
     String content;
     String name;
     String memberId;
+    
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    Date createdAt;
 }
