6faeb82e5be8a76e1db99ab66c628549f75bc015
juhyeon1202 <wngus031202@gmail.com>
2026-02-10 10:43:25 +0900

    커뮤니티 WebConfig 추가
    커뮤니티 멀티미디어 파일 접근 설정 추가

--- restfulProject/src/main/java/com/kh/spring/util/FileUtil.java
+++ restfulProject/src/main/java/com/kh/spring/util/FileUtil.java
@@ -15,7 +15,7 @@ public class FileUtil {
 	@Value("${file.upload.path}")
 	private String savePath;
 	
-	public String saveFile(MultipartFile uploadFile) throws Exception {
+	public String saveFile(MultipartFile uploadFile, String subFolder) throws Exception {
 		
 		//원본 파일명
 		String originName = uploadFile.getOriginalFilename();
@@ -32,17 +32,35 @@ public class FileUtil {
 		//합쳐주기
 		String changeName =  currentTime + ranNum + ext;
 		
+		// 저장 폴더 경로 설정 (기본 경로 + 하위 폴더)
+		String folderPath = savePath;
+		if (subFolder != null && !subFolder.isEmpty()) {
+			folderPath += subFolder + "/";
+		}
+		
+		File folder = new File(folderPath);
+		// 폴더가 없으면 생성
+		if (!folder.exists()) {
+			folder.mkdirs();
+		}
+		
 		//서버에 업로드 처리
-		uploadFile.transferTo(new File(savePath + changeName));
+		File destination = new File(folderPath + changeName);
+		uploadFile.transferTo(destination);
 		
 		return changeName;
 	}
 
 	//첨부파일 삭제
-	public boolean deleteFile(String deleteFile) {
+	public boolean deleteFile(String deleteFile, String subFolder) {
+		
+		String folderPath = savePath;
+		if (subFolder != null && !subFolder.isEmpty()) {
+			folderPath += subFolder + "/";
+		}
 		
 		//저장경로 + 저장파일명 
-		String filePath = savePath + deleteFile;
+		String filePath = folderPath + deleteFile;
 		
 		File file = new File(filePath);
 		
